3414585b888ce1dd2166b0a3309db4e880d3ea8e
sabrina-dossantos@hotmail.fr <sabrina-dossantos@hotmail.fr>
2023-05-31 22:05:50 +0200

    Add includes files | CSS | JS | env

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -1,15 +1,27 @@
 <?php
+
+// configuration phpdotenv
+require __DIR__ . '/../vendor/autoload.php';
+Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/../')->load();
+
 //--------- BDD
-$mysqli = new mysqli("localhost", "db_user", "12345", "monecommerce");
+$mysqli = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
 if ($mysqli->connect_error) die('Un problème est survenu lors de la tentative de connexion à la BDD : ' . $mysqli->connect_error);
 // $mysqli->set_charset("utf8");
 //--------- SESSION
+session_name("monecommerceID");
 session_start();
 //--------- CHEMIN
-define("RACINE_SITE", "http://" . $_SERVER['HTTP_HOST'] . "/");
+// define("RACINE_SITE", "http://" . $_SERVER['HTTP_HOST'] . "/");
+define("RACINE_SITE", "http://" . $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'] . "/");
 
 //--------- VARIABLES
 // $contenu = '';
- 
+
 //--------- AUTRES INCLUSIONS
-// require_once("fonction.inc.php");
+require_once("functions.inc.php");
+// debug($mysqli, 0);
+// Déclarer une variable d’environnement
+// putenv('NOM_VARIABLE=Le nom de ma variable d\'environnement avec <span style="font-weight:bold;">putenv(NOM_VARIABLE)</span>');
+// // ou
+// $_ENV['NOM_VARIABLE'] = 'Le nom de ma variable d\'environnement avec <span style="font-weight:bold;">$_ENV[NOM_VARIABLE]</span>';
