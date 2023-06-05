fce0becaeded3f99704b35a94000b7ff05901e97
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2023-06-05 21:44:33 +0200

    profil

--- connexion.php
+++ connexion.php
@@ -1,13 +1,19 @@
 <?php require_once("inc/init.inc.php");
 //--------------------------------- TRAITEMENTS PHP ---------------------------------//
+if (isset($_GET['action']) && $_GET['action'] == "deconnexion") {
+    session_destroy();
+}
+if (internauteEstConnecte()) {
+    header("location:profil.php");
+}
 if ($_POST) {
     $resultat = executeRequete("SELECT * FROM utilisateur WHERE pseudo='$_POST[pseudo]'");
     if ($resultat->num_rows != 0) {
-        $membre = $resultat->fetch_assoc();
-        if (password_verify($_POST['mot_de_passe'], $membre['mot_de_passe'])) {
-            foreach ($membre as $indice => $element) {
+        $utilisateur = $resultat->fetch_assoc();
+        if (password_verify($_POST['mot_de_passe'], $utilisateur['mot_de_passe'])) {
+            foreach ($utilisateur as $indice => $element) {
                 if ($indice != 'mot_de_passe') {
-                    $_SESSION['membre'][$indice] = $element;
+                    $_SESSION['utilisateur'][$indice] = $element;
                 }
             }
             header("location:profil.php");
